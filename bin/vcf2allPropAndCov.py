0db1a40f0fd8b855fef60fb278d2656bab63ebc7
gmartin <guillaume.martin@cirad.fr>
2021-02-02 08:05:09 +0100

    Adding gz extension to output file

--- bin/vcf2allPropAndCov.py
+++ bin/vcf2allPropAndCov.py
@@ -321,7 +321,7 @@ def __main__():
 			dico_group[data[1]].append(data[0])
 	file.close()
 	
-	outfile = gzip.open(PREFIX+ACCESS+'_AlleleOriginAndRatio.tab','wt')
+	outfile = gzip.open(PREFIX+ACCESS+'_AlleleOriginAndRatio.tab.gz','wt')
 	outfile1 = open(PREFIX+ACCESS+'_stats.tab','w')
 	
 	# Preparing color file
